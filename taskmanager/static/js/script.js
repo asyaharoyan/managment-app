f67db32144dcf3345e9aec248a1fb24def1e8532
Asya Haroyan <alharoyan@gmail.com>
2024-02-17 16:47:58 +0000

    Add tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,19 @@ document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
+
+    // datepicker
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // Select initilization
+    let select = document.querySelectorAll('select');
+    M.FormSelect.init(select);
+
+    // collapsible initilization
+    let collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
 });
